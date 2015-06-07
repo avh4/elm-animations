3df3133acf15ab7dedf59cf07817131587c4f1b5
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-06-07 11:50:26 -0700

    Update docs for AnimationFrame

--- AnimationFrame.elm
+++ AnimationFrame.elm
@@ -1,5 +1,6 @@
 module AnimationFrame where
-{-| -- TODO:   This library is modeled after Elm's time module. It provides Signals that
+{-| -- TODO:   this was copied from https://github.com/jwmerrill/elm-animation-frame/blob/master/src/Native/AnimationFrame.js
+This library is modeled after Elm's time module. It provides Signals that
 are synchronized with the monitor's frame rate by binding javascript's
 requestAnimationFrame. `AnimationFrame.frame` and `AnimationFrame.frameWhen` are similar
 to `(Time.fps 60)` and `(Time.fpsWhen 60)` respectively, but they more reliably
@@ -14,7 +15,7 @@ import Native.AnimationFrame
 import Signal
 import Time
 
-{-| Same as the frame function, but you can turn it on and off. Allows you
+{-| Allows you
 to do brief animations based on user input without major inefficiencies.
 The values in the resulting signal are the timestamps of the frames.
 -}
