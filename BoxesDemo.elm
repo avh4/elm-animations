4897f9e02e0e53880d5a28771b657d453f4d5d5f
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-06-07 17:07:18 -0700

    BoxesDemo: fix color interpolation

--- BoxesDemo.elm
+++ BoxesDemo.elm
@@ -4,15 +4,26 @@ import Html exposing (Html)
 import Html.Attributes as Html
 import Html.Events as Html
 import Easing
-import Color
+import Color exposing (Color, toRgb, rgba)
 import Time exposing (Time)
 import String
 import Debug
 import Animation.Last exposing (..)
-        
+
 -- --
 -- -- DEMO APP
 -- --
+
+-- https://github.com/Dandandan/Easing/pull/9
+colorEase : Easing.Interpolation Color
+colorEase from to v =
+    let
+        float = Easing.float
+        (rgb1, rgb2)     = (toRgb from, toRgb to)
+        (r1, g1, b1, a1) = (rgb1.red, rgb1.green, rgb1.blue, rgb1.alpha)
+        (r2, g2, b2, a2) = (rgb2.red, rgb2.green, rgb2.blue, rgb2.alpha)
+        float' from to v = round (float (toFloat from) (toFloat to) v) |> max 0 |> min 255
+    in rgba (float' r1 r2 v) (float' g1 g2 v) (float' b1 b2 v) (float a1 a2 v)
 
 box time (i,model) =
     let
@@ -22,7 +33,7 @@ box time (i,model) =
         [ Html.div
             [ Html.style
                 [ width size, height size
-                , background <| ease Easing.color Color.red Color.blue
+                , background <| ease colorEase Color.red Color.blue
                 , transform [rotate <| ease Easing.float 0 180]]
             , Html.onMouseEnter tbox.address (Animate i True)
             , Html.onMouseLeave tbox.address (Animate i False)
