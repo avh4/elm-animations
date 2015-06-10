5c6d40f185b1a16e37a80ace3e9389b9045dc9b6
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-06-10 00:20:18 -0700

    Add material design ButtonDemo

--- Controls.elm
+++ Controls.elm
@@ -8,29 +8,40 @@ import Maybe
 import String
 import Signal
 
-slider : String -> a -> a -> a -> a -> (String -> Signal.Message) -> Html
-slider name min max step default action =
-    Html.label []
-        [ Html.text name
-        , Html.input
-            [ Html.type' "range"
-            , Html.min <| toString min
-            , Html.max <| toString max
-            , Html.step <| toString step
-            , Html.attribute "value" <| toString default
-            , Html.on "input" Html.targetValue action
-            ] []
-        ]
+type alias Slider a =
+    { html : Html
+    , signal : Signal a
+    }
 
-intSlider name min max step default mbox =
-    slider name min max step default (String.toInt >> Result.toMaybe >> Maybe.withDefault default >> Signal.message mbox.address)
+slider : String -> a -> a -> a -> a -> (String -> a) -> Slider a
+slider name min max step default parse =
+    let
+        mbox = Signal.mailbox default
+        html = 
+            Html.label []
+                [ Html.text name
+                , Html.input
+                    [ Html.type' "range"
+                    , Html.min <| toString min
+                    , Html.max <| toString max
+                    , Html.step <| toString step
+                    , Html.attribute "value" <| toString default
+                    , Html.on "input" Html.targetValue (parse >> Signal.message mbox.address)
+                    ] []
+                ]
+    in
+        { html = html, signal = mbox.signal }
 
-floatSlider : String -> Float -> Float -> Float -> Float -> Signal.Mailbox Float -> Html
-floatSlider name min max step default mbox =
-    slider name min max step default (String.toFloat >> Result.toMaybe >> Maybe.withDefault default >> Signal.message mbox.address)
+intSlider : String -> Int -> Int -> Int -> Int -> Slider Int
+intSlider name min max step default =
+    slider name min max step default (String.toInt >> Result.toMaybe >> Maybe.withDefault default)
 
-maybeIntSlider name min max step mbox =
-    slider name min max step max (String.toInt >> Result.toMaybe >> Signal.message mbox.address)
+floatSlider : String -> Float -> Float -> Float -> Float -> Slider Float
+floatSlider name min max step default =
+    slider name min max step default (String.toFloat >> Result.toMaybe >> Maybe.withDefault default)
 
-maybeFloatSlider name min max step mbox =
-    slider name min max step max (String.toFloat >> Result.toMaybe >> Signal.message mbox.address)
+--maybeIntSlider name min max step mbox =
+--    slider name min max step max (String.toInt >> Result.toMaybe >> Signal.message mbox.address)
+
+--maybeFloatSlider name min max step mbox =
+--    slider name min max step max (String.toFloat >> Result.toMaybe >> Signal.message mbox.address)
